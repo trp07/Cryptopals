de0eb6e02d6e0aeef3ab6d65062d569ceb031826
Timoteo <phillipstr@gmail.com>
2018-07-01 20:01:03 +0300

    completes set1.challenge2

--- Set1/_set1.py
+++ Set1/_set1.py
@@ -10,3 +10,8 @@ def hex_to_base64(s:str) -> str:
     return base64.b64encode(decoded).decode('utf-8')
 
 
+def fixed_xor(s1:str, s2:str) -> str:
+    """XOR strings 's1' and 's2', returning the result as a hex-string."""
+    s1_int = int(s1, base=16)
+    s2_int = int(s2, base=16)
+    return hex(s1_int ^ s2_int)[2:]
--- Set1/test_set1.py
+++ Set1/test_set1.py
@@ -1,9 +1,19 @@
 """Test all the challenges completed so far using pytest."""
 
 from _set1 import hex_to_base64
+from _set1 import fixed_xor
 
 
-def test_challenge1_hex_to_base64():
+def test_s1ch1_hex_to_base64():
+    """Test to verify set1.challenge1."""
     given = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
     expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
     assert hex_to_base64(given) == expected
+
+
+def test_s1ch2_fixed_xor():
+    """Test to verify set1.challenge2."""
+    s1_given = '1c0111001f010100061a024b53535009181c'
+    s2_given = '686974207468652062756c6c277320657965'
+    expected = '746865206b696420646f6e277420706c6179'
+    assert fixed_xor(s1_given, s2_given) == expected
