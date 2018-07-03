221abd65f71b36ed5c663d23dbb87127b44015e4
Timoteo <phillipstr@gmail.com>
2018-07-03 21:48:18 +0300

    completes set1.challenge3

--- Set1/_set1.py
+++ Set1/_set1.py
@@ -15,3 +15,40 @@ def fixed_xor(s1:str, s2:str) -> str:
     s1_int = int(s1, base=16)
     s2_int = int(s2, base=16)
     return hex(s1_int ^ s2_int)[2:]
+
+
+def xor_string(s:str, b:str) -> str:
+    """
+    XOR of hexstring 's' with hexstring 'b'.  If len(b) != len(s), then it is
+    expanded and only b[0:len(s)] is XOR'd against 's'.
+    Returns result of XOR.
+    """
+    expanded_b = b * (len(s) // 2)
+    return fixed_xor(s, expanded_b[0:len(s)])
+
+
+def decode_str(s:str) -> str:
+    """Decode the hexstring 's' into ASCII."""
+    decoded = ''
+    for i in range(0, len(s)-1, 2):
+        decoded += chr(int(s[i] + s[i+1], base=16))
+    return decoded
+
+
+def freq_count(s:str) -> int:
+    """
+    Does a basic frequency count of the decoded-string 's' based on the
+    preponderance of the letters ETAOIN SHRDLU in the English language.
+    Returns an integer of the count.
+    """
+    count = 0
+    for letter in s:
+        if letter.upper() in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+            count += 4
+        elif letter in ('0123456789., \n\t'):
+            count += 2
+        elif letter in ('?!/-'):
+            count += 1
+    return count
+
+
--- Set1/test_set1.py
+++ Set1/test_set1.py
@@ -2,6 +2,9 @@
 
 from _set1 import hex_to_base64
 from _set1 import fixed_xor
+from _set1 import xor_string
+from _set1 import decode_str
+from _set1 import freq_count
 
 
 def test_s1ch1_hex_to_base64():
@@ -17,3 +20,20 @@ def test_s1ch2_fixed_xor():
     s2_given = '686974207468652062756c6c277320657965'
     expected = '746865206b696420646f6e277420706c6179'
     assert fixed_xor(s1_given, s2_given) == expected
+
+
+def test_s1ch3_single_byte_xor():
+    """Test to verify set1.challenge3."""
+    c = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    expected = "Cooking MC's like a pound of bacon"
+    results = {}
+    for i in range(0, 256):
+        xord = xor_string(c, hex(i)[2:])
+        decoded = decode_str(xord)
+        fcount = freq_count(decoded)
+        results[i] = (fcount, decoded)
+    assert results[max(results, key=results.get)][1] == expected
+
+
+
+
